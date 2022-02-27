cd0f569f5bb43e9755ec05fdbb4c97c20e77e5ea
Challenger232 <challenger7billy@gmail.com>
2022-02-27 14:04:17 -0500

    include stops, update ui

--- src/SubwayLines.js
+++ src/SubwayLines.js
@@ -8,16 +8,24 @@ class SubwayLines extends Component {
         this.state = {
             routes : [],
             stops : [],
+            currentRoute : "",
         };
     }
 
     componentDidMount () {
         this.getSubwayLines()
-        this.getSubwayStops()
     }
 
-    getSubwayStops() {
-        // alert('Button Clicked');
+    getSubwayStops(id, name) {
+        this.setState({ currentRoute : name})
+        axios.get('https://api-v3.mbta.com/stops?filter[route]='+id)
+            .then(response => {
+                this.setState({ stops: response.data.data})
+                console.log(response.data.data)
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     getSubwayLines() {
@@ -32,22 +40,36 @@ class SubwayLines extends Component {
 
     render() {
         return (
-            <aside className="w-96">
-                <div className="px-3 py-4 overflow-y-auto rounded bg-gray-200 dark:bg-gray-800">
-                    { this.state.routes === null && <p>Loading routes...</p> }
-                    {
-                        this.state.routes &&
-                        this.state.routes.map(data=> (
-                            <div key={data.id}>
-                                 <button className="flex flex-col items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 w-full">
-                                     <span className="">ID: {data.id}</span>
-                                     <span>Name: {data.attributes.long_name}</span>
-                                 </button>
-                            </div>
-                        ))
-                    }
+            <div className="flex flex-row">
+                <div className="flex w-1/4">
+                    <div className="px-3 py-4 overflow-y-auto rounded bg-gray-200 dark:bg-gray-800 w-full">
+                        { this.state.routes === null && <p>Loading routes...</p> }
+                        {
+                            this.state.routes &&
+                            this.state.routes.map(data=> (
+                                <div key={data.id}>
+                                    <button className="flex flex-col items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 w-full"
+                                            onClick={() => this.getSubwayStops(data.id, data.attributes.long_name)}>
+                                        <span className="">ID: {data.id}</span>
+                                        <span>Name: {data.attributes.long_name}</span>
+                                    </button>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
-            </aside>
+                <div className="flex w-3/4 flex flex-col">
+                    <h1 className="text-gray-800 text-2xl px-4 font-bold flex">
+                        { this.state.currentRoute && <h1>{this.state.currentRoute} Stops</h1> }
+                    </h1>
+                    <div className="grid grid-cols-4 gap-4 flex p-4">
+                        { this.state.stops === null && <p>Loading stops...</p> }
+                        { this.state.stops && this.state.stops.map(data => (
+                            <h1 key={data.id}>{data.attributes.name}</h1>
+                        ))}
+                    </div>
+                </div>
+            </div>
         );
     }
 }
